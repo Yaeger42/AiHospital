4a40cbef8824e8476c8653d29d0129f645e7d9a6
Diego <projectdcd@gmail.com>
2020-04-04 00:43:36 -0600

    Settings modified for static files and login

--- aihospital/aihospital/settings.py
+++ aihospital/aihospital/settings.py
@@ -113,8 +113,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/static/img/'
+LOGIN_URL = '/users/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = LOGIN_URL
