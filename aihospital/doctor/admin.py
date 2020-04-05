6a86baf7bf117405ca35dae1971a392aeb66fa2e
Diego <projectdcd@gmail.com>
2020-04-05 11:33:49 -0500

    Added pycache to gitignore

--- aihospital/doctor/admin.py
+++ aihospital/doctor/admin.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from django.contrib.auth.models import User
+from doctor.models import Profile
+
+
+@admin.register(Profile)
+class ProfileDoctorAdmin(admin.ModelAdmin):
+    """"Doctor admin profile"""
+    list_display = ('pk', 'user', 'phoneNumber', 'specialty', 'cedID', 'bday')
+    list_display_links = ('pk', 'user', 'cedID')
+    list_editable = ('phoneNumber', 'specialty')
+
+    search_fields = ('cedID', 'user__username', 'user__email', 'specialty')
+
+
+class ProfileDoctorAdminInLine(admin.StackedInline):
+    """Add profile user admin to base user admin"""
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    """Add profile admin to base user admin."""
+
+    inlines = (ProfileDoctorAdminInLine,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+
+admin.site.unregister(User)
+admin.site.register(User, ProfileDoctorAdmin)
